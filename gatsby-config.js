bc2d4ff5b1ae3ce87776d054befcb1334c559b5a
jonahdc <jonahlogy@gmail.com>
2019-04-14 19:11:12 +0800

    Typescript and plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
   // Note: it must *not* have a trailing slash.
   pathPrefix: `/blog`,
+  plugins: [`gatsby-plugin-typescript`],
 }
---
+++ gatsby-node.js
@@ -0,0 +1,45 @@
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create blog post pages.
+    result.data.allMarkdownRemark.edges.forEach(edge => {
+      createPage({
+        // Path for this page — required
+        path: `${edge.node.fields.slug}`,
+        component: blogPostTemplate,
+        context: {
+          // Add optional context data to be inserted
+          // as props into the page component..
+          //
+          // The context data can also be used as
+          // arguments to the page GraphQL query.
+          //
+          // The page "path" is always available as a GraphQL
+          // argument.
+        },
+      })
+    })
+  })
+}
